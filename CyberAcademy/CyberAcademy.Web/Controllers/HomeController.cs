f3848a634246ae2d99a517a473d67b9d2f950770
Prolifik Lexzy <pro_lifik@live.com>
2018-04-05 13:41:01 +0100

    [Prolifk] Code get started.

---
+++ CyberAcademy/CyberAcademy.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyberAcademy.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult SomePage()
+        {
+            return PartialView();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            this.ViewData["Male"] = Url.Content("~/images/profile.png");
+
+            this.ViewData["Female"] = Url.Content("~/images/profile2.png");
+
+            this.ViewBag.Styles = "border: 1px solid green;";
+            return View();
+        }
+
+    }
+}
---
+++ CyberAcademy/CyberAcademy.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CyberAcademy.Web.MvcApplication" Language="C#" %>
