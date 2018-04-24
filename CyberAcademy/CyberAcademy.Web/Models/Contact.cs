cc02f7e25a30b0f62859e3002c5a4f72b2273242
Prolifik Lexzy <pro_lifik@live.com>
2018-04-24 13:06:38 +0100

    [Prolifik]

--- CyberAcademy/CyberAcademy.Web/Models/Contact.cs
+++ CyberAcademy/CyberAcademy.Web/Models/Contact.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CyberAcademy.Web.Models
 {
-    public class Contact
+    public class Contact : IdentityUser
     {
-        public string ProfileImage { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Product { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public bool IsActive { get; set; }
+        private Contact()
+        {
+
+        }
+
+        public static Contact Create(string username, DateTime createdOn)
+        {
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException("username is empty");
+
+
+            return new Contact()
+            {
+                UserName = username,
+                Name = "Prolifik Lexzy",
+                Email = username,
+                IsActive = true,
+                EmailConfirmed = true,
+                CreatedOn = createdOn
+            };
+        }
+
+        public string ProfileImage { get; private set; }
+        public string Name { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public bool IsActive { get; private set; }
     }
 }
